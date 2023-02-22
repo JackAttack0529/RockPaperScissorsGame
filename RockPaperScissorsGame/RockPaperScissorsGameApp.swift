e7f86d9569d903ce200415aac08582fcda71c31c
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-02-22 08:32:36 -0600

    Initial Commit

---
+++ RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Jack Hyde on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
